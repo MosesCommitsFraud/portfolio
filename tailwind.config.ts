b5dc1ad3f9911bb51cfad7c02f34d16c7aa8fedd
Moritz Schäfer <154064199+MosesCommitsFraud@users.noreply.github.com>
2025-05-27 16:33:35 +0200

    changed font to fit the cyberpunk theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        rajdhani: ["var(--font-rajdhani)", "system-ui", "sans-serif"],
+        sans: ["var(--font-rajdhani)", "system-ui", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
